c31afe26f1367b27cdbee71d5d955f00faaf5ba3
preti14 <preti14@gmail.com>
2020-07-28 04:36:36 -0700

    verbose 2

--- home/models.py
+++ home/models.py
@@ -5,11 +5,12 @@ from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
+    """ Home page models"""
     templates = "home/home_page.html"
     #max_length = 0
     banner_title = models.CharField(max_length=100,blank=False,null=True)
     content_panels = Page.content_panels + [ FieldPanel("banner_title")]
 
     class Meta:
-        verbose_name = "Single home"
-        verbose_name_plural = "Multiple home page"
+        verbose_name = "Single Home"
+        verbose_name_plural = "Multiple Home page"
