65707a3a7d998192f128eceb6f92082592f8c56a
preti14 <preti14@gmail.com>
2020-07-28 04:26:47 -0700

    commit7

--- home/models.py
+++ home/models.py
@@ -6,6 +6,6 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     templates = "home/home_page.html"
-    max_length = 1
+    max_length = 0
     banner_title = models.CharField(max_length=100,blank=False,null=True)
     content_panels = Page.content_panels + [ FieldPanel("banner_title")]
