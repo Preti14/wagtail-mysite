0b31f4656fcf45c6e6d66bfb33c214e298eecff7
preti14 <preti14@gmail.com>
2020-07-29 17:28:38 -0700

    only view home page

--- home/models.py
+++ home/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
+
 from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     """ Home page models    """
     templates = "home/home_page.html"
-    #max_length = 1
+    max_count = 1
     banner_title = models.CharField(max_length=100,blank=False,null=True)
-    banner_subtitle = RichTextField(features=["bold","italic"])
+    #banner_subtitle = RichTextField(features=["bold","italic"])
     content_panels = Page.content_panels + [FieldPanel("banner_title")]
 
     class Meta:
