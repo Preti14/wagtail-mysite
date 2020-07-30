2ff84f7d87fcc868c8a85afffb1adb05a68d03b3
preti14 <preti14@gmail.com>
2020-07-30 07:58:51 -0700

    only view home page

--- home/models.py
+++ home/models.py
@@ -11,7 +11,20 @@ class HomePage(Page):
     max_count = 1
     banner_title = models.CharField(max_length=100,blank=False,null=True)
     banner_subtitle = RichTextField(features=["bold","italic"])
-    #banner_image = models.for
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
     content_panels = Page.content_panels + [FieldPanel("banner_title")]
 
     class Meta:
